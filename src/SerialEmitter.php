2a9b4180bc38b653ea19a2a9fa0d17a0ce0c0996
Mikk Tendermann <mikk150@gmail.com>
2017-02-18 01:56:12 +0200

    do not deal with config myself, because appearently I am too noob for that

--- src/SerialEmitter.php
+++ src/SerialEmitter.php
@@ -11,40 +11,20 @@ use React\EventLoop\LoopInterface;
 */
 class SerialEmitter extends EventEmitter
 {
-    const CHARLENGTH_8 = 8;
-    const CHARLENGTH_7 = 7;
-    const CHARLENGTH_6 = 6;
-    const CHARLENGTH_5 = 5;
-
-    const STOPBITS_1 = 1;
-    const STOPBITS_2 = 2;
-
-    const FLOW_NONE = 0;
-    const FLOW_RTC_CTS = 1;
-    const FLOW_XON_XOFF = 2;
-
-    const PAIRITY_NONE = 0;
-    const PAIRITY_ODD = 1;
-    const PAIRITY_EVEN = 2;
-
     protected $loop;
     
     protected $fileStreamer;
 
     /**
-     * Creates serialemitter
+     * Creates serialEmitter
      *
-     * @param      \React\EventLoop\LoopInterface  $loop        The loop
-     * @param      integer                         $device      The device
-     * @param      integer                         $baudrate    The baudrate
-     * @param      integer                         $pairity     The pairity
-     * @param      integer                         $flow        The flow
-     * @param      integer                         $stopbits    The stopbits
-     * @param      integer                         $charlength  The charlength
+     * @param      \React\EventLoop\LoopInterface  $loop    Loop
+     * @param      string                          $device  Device
+     * @param      string                          $config  Device configuration
      */
-    public function __construct(LoopInterface $loop, $device, $baudrate, $pairity = self::PAIRITY_ODD, $flow = self::FLOW_NONE, $stopbits = self::STOPBITS_1, $charlength = self::CHARLENGTH_8)
+    public function __construct(LoopInterface $loop, $device, $config)
     {
-        $this->configureDevice($device, $baudrate, $pairity, $flow, $stopbits, $charlength);
+        $this->configureDevice($device, $config);
 
         $stream = fopen($device, 'rw+');
 
@@ -68,55 +48,10 @@ class SerialEmitter extends EventEmitter
         $this->fileStreamer->write($data);
     }
 
-    protected function configureDevice($device, $baudrate, $pairity, $flow, $stopbits, $charlength)
+    protected function configureDevice($device, $config)
     {
-        $this->exec('stty -F ' . $device . ' ' . $baudrate);
-        switch ($pairity) {
-            case self::PAIRITY_NONE:
-                $this->exec('stty -F ' . $device . ' -parenb');
-                break;
-            case self::PAIRITY_ODD:
-                $this->exec('stty -F ' . $device . ' parenb parodd');
-                break;
-            case self::PAIRITY_EVEN:
-                $this->exec('stty -F ' . $device . ' parenb -parodd');
-                break;
-        }
-        switch ($flow) {
-            case self::FLOW_NONE:
-                $this->exec('stty -F ' . $device . ' clocal -crtscts -ixon -ixoff');
-                break;
-            case self::FLOW_XON_XOFF:
-                $this->exec('stty -F ' . $device . ' -clocal -crtscts ixon ixoff');
-                break;
-            case self::FLOW_RTC_CTS:
-                $this->exec('stty -F ' . $device . ' -clocal crtscts -ixon -ixoff');
-                break;
-        }
-        switch ($stopbits) {
-            case self::STOPBITS_1:
-                $this->exec('stty -F ' . $device . ' -cstopb');
-                break;
-            case self::STOPBITS_2:
-                $this->exec('stty -F ' . $device . ' cstopb');
-                break;
-        }
-        switch ($charlength) {
-            case self::CHARLENGTH_8:
-                $this->exec('stty -F ' . $device . ' cs8');
-                break;
-            case self::CHARLENGTH_7:
-                $this->exec('stty -F ' . $device . ' cs7');
-                break;
-            case self::CHARLENGTH_6:
-                $this->exec('stty -F ' . $device . ' cs6');
-                break;
-            case self::CHARLENGTH_5:
-                $this->exec('stty -F ' . $device . ' cs5');
-                break;
-        }
+        $this->exec('stty -F ' . $device . ' ' . $config);
     }
-
 
     protected function handleData($data)
     {
